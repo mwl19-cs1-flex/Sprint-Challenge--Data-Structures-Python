48041d269d898055d0800668a228d3865381c753
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-03-27 08:14:17 -0700

    Completed reverse

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -47,4 +47,19 @@ class LinkedList:
 
     def reverse_list(self, node, prev):
         # You must use recursion for this solution
-        pass
+        # pass
+        current = self.head
+        # sets self.head to variable current
+        prev = None
+        # sets none to variable prev
+        while current:
+            next = current.next_node
+            # sets next of current to variable next
+            current.next_node = prev
+            # sets prev to variable next of current
+            prev = current
+            # sets current to variable prev
+            current = next
+            # sets next to variable of current
+        self.head = prev 
+        # once the while loop is broken, set prev to self.head
