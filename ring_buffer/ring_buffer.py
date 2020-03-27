81cb5defcdfa97fd4495b1812e804b0e96871382
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-03-27 10:56:14 -0700

    Commit for sprint retro, ring buffer not completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,26 +7,45 @@ class RingBuffer:
         self.current = None
         self.storage = DoublyLinkedList()
 
+    # THIS DOES NOT WORK
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            node = self.current
+            self.storage.delete(node)
+            self.storage.add_to_head(item)
+            self.current = self.storage.head
+        else:
+            if self.current is None:
+                self.storage.add_to_head(item)
+                self.current = self.storage.head
+            else:
+                self.storage.add_to_tail(item)
+                self.current = self.storage.tail
+                # self.storage.head = self.storage.head.next
+                # self.current = self.storage.head
 
+    # THIS WORKS
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        self.current = self.storage.head
+        current = self.current
+        while current:
+            list_buffer_contents.append(current.value)
+            current = current.next
 
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
 
 
-class ArrayRingBuffer:
-    def __init__(self, capacity):
-        pass
+# class ArrayRingBuffer:
+#     def __init__(self, capacity):
+#         pass
 
-    def append(self, item):
-        pass
+#     def append(self, item):
+#         pass
 
-    def get(self):
-        pass
+#     def get(self):
+#         pass
